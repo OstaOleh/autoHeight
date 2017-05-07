086ce5419d41860abf94252a786fcf2231be0f6a
OstaOleh <ostaolegg@gmail.com>
2017-05-07 18:13:06 +0300

    rework

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-var k = jQuery.noConflict();
 
 function equalHeight(group) {
 	var tallest = 0;
-	group.each(function() {
-		thisHeight = k(this).height();
-		if (thisHeight > tallest) {
-			tallest = thisHeight;
-		}
-	});
-	group.height(tallest);
+	group
+		.each(function() {
+			tallest = Math.max(tallest, $(this).height());
+		})
+		.height(tallest);
 }
-k(document).ready(function() {
-	equalHeight(k("some class or id"));
-	equalHeight(k("some class or id"));
+$(function() {
+	equalHeight($('some class or id'));
 
 });
